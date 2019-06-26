516e58ebc192d234536b4f6e2841058c7b32a260
gbox3d <gbox3d@gmail.com>
2019-06-26 15:41:54 +0900

    예제 3번

---
+++ day3/exam3/exam3.cpp
@@ -0,0 +1,21 @@
+﻿// exam3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    //std::cout << "Hello World!\n"; 
+	int num1,num2,result;
+
+	printf_s("두수를 입력하세요. => ");
+	scanf_s("%d %d",&num1,&num2);
+
+	result = (num1 + num2) / 2;
+
+	printf_s("결과는 : %d \n", result);
+
+	return 0;
+}
