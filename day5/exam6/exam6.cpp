f4cb0cf6a2235d69f43a02c9bb381722546e66de
gbox3d <gbox3d@gmail.com>
2019-06-28 15:54:35 +0900

    exam6 증감연산자 예제

---
+++ day5/exam6/exam6.cpp
@@ -0,0 +1,36 @@
+﻿// exam6.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	char _cmd;
+	int _bLoop = 1;
+	while (_bLoop)
+	{
+		printf_s("커멘드 입력(도움말 h) :");
+		scanf_s("\n%c", &_cmd,sizeof(_cmd));
+		switch (_cmd)
+		{
+		case 'q':
+		{
+			_bLoop = 0; printf_s("\n게임을 종료합니다.\n");
+		}
+			break;
+		case 'h':
+		{
+			printf_s("m. 돈벌기\n");
+			printf_s("h. 치료하기\n");
+			printf_s("u. 사냥하기\n");
+			printf_s("g. 아이템 줍기\n");
+			printf_s("q. quit\n");
+		}
+			break;
+		default:
+			break;
+		}
+	}
+}
+
