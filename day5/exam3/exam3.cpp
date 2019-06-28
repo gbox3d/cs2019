fb1f6c900be5ba1f531d5c44dee316333f7b8213
gbox3d <gbox3d@gmail.com>
2019-06-28 14:04:20 +0900

    exam4 switch 문예제

---
+++ day5/exam3/exam3.cpp
@@ -0,0 +1,35 @@
+﻿// exam3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	int num = 45; //정답
+	int _input;
+	printf_s("-- 숫자 맞추기 게임 -- \n 아무 숫자나 입력하세요 :");
+
+	scanf_s("%d", &_input);
+
+	if (num == _input) {
+		printf_s("당신이 이겼습니다. \n");
+	}
+	//if (num != _input) {
+	else {
+		printf_s("당신이 졌습니다. \n");
+
+		if (num < _input) {
+			printf_s("hint! : 왜냐구요? 너무 크기 때문입니다. \n");
+		}
+		//if (num > _input) {
+		else {
+			printf_s("hint! : 왜냐구요? 너무 작기 때문입니다. \n");
+		}
+	}
+
+	printf_s("게임종료 \n");
+	return 0;
+    
+}
+
---
+++ day5/exam4/exam4.cpp
@@ -0,0 +1,39 @@
+﻿// exam4.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+	printf_s("캐릭터 직업을 고르세요. \n");
+	printf_s("1. 파이터 \n");
+	printf_s("2. 마법사 \n");
+	printf_s("3. 치료사 \n");
+
+	int _selectChr = 0;
+
+	scanf_s("%d", &_selectChr );
+
+	printf_s("선택한 번호 : %d \n", _selectChr);
+	
+	switch (_selectChr)
+	{
+	case 1:
+		printf_s("파이터가 생성되었습니다. \n");
+		break;
+	case 2:
+		printf_s("마법사가 생성되었습니다. \n");
+		break;
+	case 3:
+		printf_s("치료사가 생성되었습니다. \n");
+		break;
+	default:
+		printf_s("번호를 잘못 입력하셨어요. \n");
+		break;
+	}
+
+	return 0;
+
+
+    //std::cout << "Hello World!\n";
+}
