0b164ca2d3cc4d65a8307dee0ff33d659421cf66
gbox3d <gbox3d@gmail.com>
2019-06-28 10:29:56 +0900

    day5

---
+++ day5/exam1/exam1.cpp
@@ -0,0 +1,19 @@
+﻿// exam1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+	
+	char a, b,c;
+	//scanf_s("%c  %c",&a,sizeof(a),&b,sizeof(b));
+	
+	printf_s("문자 3개 입력하세요 : ");	
+	scanf_s("%c\n", &a, sizeof(a));	
+	scanf_s("%c\n", &b, sizeof(b));
+	scanf_s("%c", &c, sizeof(c));
+	printf_s("=> %c %c %c\n",a,b,c);
+
+}
+
