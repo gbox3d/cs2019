f626f25260b083b0752f5e6d905572cb324e4915
gbox3d <gbox3d@gmail.com>
2019-06-28 16:56:59 +0900

    exam6 증감 연산자

--- day5/exam6/exam6.cpp
+++ day5/exam6/exam6.cpp
@@ -8,6 +8,8 @@ int main()
 {
 	char _cmd;
 	int _bLoop = 1;
+	int _nMoney = 0;
+	int _nMoneyPower = 1;
 	while (_bLoop)
 	{
 		printf_s("커멘드 입력(도움말 h) :");
@@ -15,7 +17,7 @@ int main()
 		switch (_cmd)
 		{
 		case 'q':
-		{
+		{	
 			_bLoop = 0; printf_s("\n게임을 종료합니다.\n");
 		}
 			break;
@@ -27,6 +29,18 @@ int main()
 			printf_s("g. 아이템 줍기\n");
 			printf_s("q. quit\n");
 		}
+			break;
+		case 'g':
+		{
+			printf_s("곡괭이를 주웠습니다.\n");
+			_nMoneyPower = 10;
+		}
+			break;
+		case 'm':
+			//돈벌기 처리....
+			_nMoney = _nMoney + _nMoneyPower;
+			printf_s("현재 보유금액 : %d \n", _nMoney);
+
 			break;
 		default:
 			break;
