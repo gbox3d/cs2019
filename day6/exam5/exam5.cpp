fdbad424823e8c95d75844f4e5883bdd52786ec4
gbox3d <gbox3d@gmail.com>
2019-07-01 15:02:19 +0900

    ....

--- day6/exam5/exam5.cpp
+++ day6/exam5/exam5.cpp
@@ -2,19 +2,64 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+
+int _sum(int a, int b)
+{
+	int result = a + b;
+	return result;
+}
+
+int _sub(int a, int b)
+{
+	int result = a - b;
+	return result;
+}
+
+int _mul(int a, int b)
+{
+	int result = a * b;
+	return result;
+}
+int _div(int a, int b)
+{
+	int result = a / b;
+	return result;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int a, b;
+	char _operator;
+
+	scanf_s("%d %c %d", &a, &_operator, sizeof(_operator), &b); // 3 + 5 
+	printf_s("%d,%c,%d\n", a, _operator, b);
+	switch (_operator)
+	{
+	case '+':
+	{	
+		printf_s("두수의 합은 : %d\n",_sum(a,b));
+	}
+		break;
+	case '-':
+	{	
+		printf_s("두수의 차는 : %d\n",_sub(a,b));
+	}
+	break;
+	case '*':
+	{	
+		printf_s("두수의 곱은 : %d\n", _mul(a,b));
+	}
+	break;
+	case '/':
+	{	
+		printf_s("두수의 몫은 : %d\n", _div(a,b));
+	}
+	break;
+	default:
+		break;
+	}
+
+    
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
