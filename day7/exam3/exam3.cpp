2b0a60198a7c20c336acbbb9e5230579ccf2bc65
gbox3d <gbox3d@gmail.com>
2019-07-02 11:31:09 +0900

    stack

---
+++ day7/exam3/exam3.cpp
@@ -0,0 +1,41 @@
+﻿// exam3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+int nBuf[256];
+int nLastIndex = -1;
+void printData()
+{
+	for (int i = 0; i <= nLastIndex; i++)
+	{
+		printf_s("%d ", nBuf[i]);
+	}
+}
+void pushData(int _data)
+{
+	nLastIndex++;
+	nBuf[nLastIndex] = _data;
+}
+
+int main()
+{
+	int _bLoop = 1;
+	while (_bLoop)
+	{
+		int _cmd; printf_s("Cmd>"); scanf_s("%d", &_cmd);
+		switch (_cmd) 
+		{
+		case 0: _bLoop = 0; printf_s("종료합니다.\n"); break;
+		case 1: {int _num; printf_s("input num =>"); scanf_s("%d", &_num); pushData(_num); }  break; //push
+		case 2: 
+		break; //pop
+		case 3: printData(); break; //show
+		}
+
+	}
+
+	
+
+}
