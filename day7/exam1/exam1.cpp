03d43e63c54ca2e036d0d662db47771b309a0bb7
gbox3d <gbox3d@gmail.com>
2019-07-02 10:19:22 +0900

    day7 add

---
+++ day7/exam1/exam1.cpp
@@ -0,0 +1,29 @@
+﻿// exam1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int gold[5] = { 120,130,100,110,90 };
+	/*
+	int gold[5];
+	gold[0] = 120;
+	gold[1] = 130;
+	gold[2] = 100;
+	gold[3] = 110;
+	gold[4] = 90;
+	*/
+
+	printf_s("gold[0] : %d \n", gold[0]);
+	printf_s("sizeof(gold) : %d , sizeof(int) :%d \n", sizeof(gold), sizeof(int));
+
+	for (int i=0; i < sizeof(gold)/sizeof(int); i++)
+	{
+		printf_s("gold[%d] : %d \n",i, gold[i]);
+	}
+
+	return 0;
+    //std::cout << "Hello World!\n";
+}
+
---
+++ day7/exam2/exam2.cpp
@@ -0,0 +1,60 @@
+﻿// exam2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void showData(int _buf[],int nBufLength)
+{
+	for (int i = 0; i < nBufLength; i++)
+	{
+		if (_buf[i] == -1) break;
+		printf_s("%d ", _buf[i]);
+	}
+}
+
+void pushData(int _buf[], int nBufLength, int _data)
+{
+	for (int i = 0; i < nBufLength; i++)
+	{
+		if (_buf[i] == -1) {
+			_buf[i + 1] = -1;
+			_buf[i] = _data;
+			break;
+		}
+	}
+}
+int main()
+{
+	int nBuf[256] = {-1};
+	int nBufLength = sizeof(nBuf) / sizeof(int);
+
+	nBuf[0] = 3;
+	nBuf[1] = -1;
+
+	int _cmd;
+	int _bLoop = 1;
+	while (_bLoop)
+	{
+		printf_s("\nCmd>"); scanf_s("%d", &_cmd);
+		switch (_cmd)
+		{
+		case 0:
+			_bLoop = 0;
+			printf_s("프로그램을 종료 합니다. \n");
+			break;
+		case 1:
+			showData(nBuf, nBufLength);
+			break;
+		case 2:
+			pushData(nBuf, nBufLength,3);
+			break;
+		case 3:
+			//popData(nBuf, nBufLength);
+			break;
+		default:
+			break;
+		}
+	}
+	
+	return 0;
+}
