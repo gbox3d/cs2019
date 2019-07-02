98eb031565cf72f0eed5797b75e602bc573122d2
gbox3d <gbox3d@gmail.com>
2019-07-02 11:06:57 +0900

    push pop

--- day7/exam2/exam2.cpp
+++ day7/exam2/exam2.cpp
@@ -7,7 +7,14 @@ void showData(int _buf[],int nBufLength)
 {
 	for (int i = 0; i < nBufLength; i++)
 	{
-		if (_buf[i] == -1) break;
+		if (_buf[i] == -1)
+		{
+			if (i <= 0) 
+			{
+				printf_s("데이터가 없습니다. \n");
+			}
+			break;
+		}
 		printf_s("%d ", _buf[i]);
 	}
 }
@@ -23,6 +30,28 @@ void pushData(int _buf[], int nBufLength, int _data)
 		}
 	}
 }
+
+int popData(int _buf[], int nBufLength)
+{
+	for (int i = 0; i < nBufLength; i++)
+	{
+		if (_buf[i] == -1) {
+			if(i > 0)
+			{
+				int _r = _buf[i - 1];
+				_buf[i - 1] = -1;
+				return _r;
+			}		
+			else
+			{
+				printf_s("지워질 데이터가 없습니다.\n");
+			}
+			break;
+		}
+	}
+	return -1;
+}
+
 int main()
 {
 	int nBuf[256] = {-1};
@@ -35,7 +64,7 @@ int main()
 	int _bLoop = 1;
 	while (_bLoop)
 	{
-		printf_s("\nCmd>"); scanf_s("%d", &_cmd);
+		printf_s("\n(1.show 2.push 3.pop)Cmd>"); scanf_s("%d", &_cmd);
 		switch (_cmd)
 		{
 		case 0:
@@ -46,10 +75,14 @@ int main()
 			showData(nBuf, nBufLength);
 			break;
 		case 2:
-			pushData(nBuf, nBufLength,3);
+		{
+			int _num;
+			printf_s("\ninput number =>"); scanf_s("%d", &_num);
+			pushData(nBuf, nBufLength, _num);
+		}	
 			break;
 		case 3:
-			//popData(nBuf, nBufLength);
+			printf_s("pop : %d \n", popData(nBuf, nBufLength) );
 			break;
 		default:
 			break;
