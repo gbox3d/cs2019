f59d8ad180eae5ef64b1d582baff2a3f39b93720
gbox3d <gbox3d@gmail.com>
2019-07-02 11:50:41 +0900

    ...

--- day7/exam3/exam3.cpp
+++ day7/exam3/exam3.cpp
@@ -11,6 +11,7 @@ void printData()
 	for (int i = 0; i <= nLastIndex; i++)
 	{
 		printf_s("%d ", nBuf[i]);
+		
 	}
 }
 void pushData(int _data)
@@ -18,24 +19,30 @@ void pushData(int _data)
 	nLastIndex++;
 	nBuf[nLastIndex] = _data;
 }
-
+int popData()
+{
+	int _r=-9999;
+	if (nLastIndex < 0) { printf_s("데이터가 없음."); }
+	else 
+	{
+		_r = nBuf[nLastIndex];
+		nLastIndex--;
+	}
+	return _r;
+}
 int main()
 {
 	int _bLoop = 1;
 	while (_bLoop)
 	{
-		int _cmd; printf_s("Cmd>"); scanf_s("%d", &_cmd);
+		int _cmd; printf_s("\n Cmd>"); scanf_s("%d", &_cmd);
 		switch (_cmd) 
 		{
 		case 0: _bLoop = 0; printf_s("종료합니다.\n"); break;
 		case 1: {int _num; printf_s("input num =>"); scanf_s("%d", &_num); pushData(_num); }  break; //push
-		case 2: 
-		break; //pop
+		case 2: {printf_s("pop : %d\n",popData()); } break; //pop
 		case 3: printData(); break; //show
 		}
 
 	}
-
-	
-
 }
