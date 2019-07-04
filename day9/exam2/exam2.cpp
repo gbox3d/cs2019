8bb215adbd98cfe6336d05531a03ac777df07f3c
gbox3d <gbox3d@gmail.com>
2019-07-04 13:56:05 +0900

    ....

---
+++ day9/exam2/exam2.cpp
@@ -0,0 +1,23 @@
+﻿// exam2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int a,b;
+	int* pA;
+	int* pB;
+
+	a = 3;
+	b = 4;
+
+	pA = &a;
+	*pA = b; // a=b
+
+	printf_s("%d %d\n",*pA,a);
+
+
+	return 0;
+}
+
---
+++ day9/exam3/exam3.cpp
@@ -0,0 +1,23 @@
+﻿// exam3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+//1씩 증가 시키는 함수
+void incNumber(int* _a) 
+{	
+	*_a = *_a + 1;
+	//printf_s("passed a %d \n", _a);
+}
+
+int main()
+{
+	int a, b;
+	a = 0;
+	b = 4;
+	printf_s("%d \n", a);//0
+
+	incNumber(&a);
+	printf_s("%d \n", a);//1
+    
+}
