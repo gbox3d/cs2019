d5995cf29273e172bcd495975bc610f6fc060b00
gbox3d <gbox3d@gmail.com>
2019-07-05 13:29:31 +0900

    ...

---
+++ day10/exam2/exam2.cpp
@@ -0,0 +1,29 @@
+﻿// exam2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include "..\..\..\cstudy\engine\tge.h"
+
+void sample1();
+
+int main()
+{
+	int bLoop = true;
+
+	while (bLoop)
+	{
+		char szBuf[256];
+		gets_s(szBuf);
+		int tokenNum = TGE::Tokenize(szBuf);
+		if (!strcmp("exit", TGE::g_szTokens[0])) {
+			bLoop = false;
+			puts("exit program....");
+		}
+		else if (!strcmp("call", TGE::g_szTokens[0])) {
+			if (!strcmp("sample1", TGE::g_szTokens[1])) {
+				sample1();
+			}
+		}
+	}
+	return 0;
+}
---
+++ day10/exam2/sample1.cpp
@@ -0,0 +1,26 @@
+#include "..\..\..\cstudy\engine\tge.h"
+
+void sample1()
+{
+	puts("sample 1 start ");
+
+	int score[3][4] = {
+		{1,2,3,4} ,
+		{10,20,30,40},
+		{11,12,13,14}
+	};
+
+	//[За(y)][ї­(x)]
+	printf_s("%d \n",score[1][0]);
+
+	for (int row = 0; row < 3; row++)
+	{
+		for (int col = 0; col < 4; col++)
+		{
+			printf_s("%d \n", score[row][col]);
+		}
+	}
+
+
+
+}
