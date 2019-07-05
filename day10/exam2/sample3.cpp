fd59e23cbbe42f34753e187359b5f6c42e83cd65
gbox3d <gbox3d@gmail.com>
2019-07-05 15:51:27 +0900

    ...

--- day10/exam2/sample3.cpp
+++ day10/exam2/sample3.cpp
@@ -4,31 +4,6 @@ void sample3()
 {
 	HANDLE _hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	int _matObj1[4][4] = {
-		
-	};
-
-	/*
-	TGE::g_chiBuffer[0].Char.AsciiChar = 'A';
-	TGE::g_chiBuffer[0].Attributes = 0x00f0;
-
-	TGE::g_chiBuffer[1].Char.AsciiChar = 'B';
-	TGE::g_chiBuffer[1].Attributes = 0x00f0;
-	*/
-
-	/*
-	for (int i = 0; i < 25; i++) {
-		TGE::g_chiBuffer[i].Char.AsciiChar = 'A'+i;
-		TGE::g_chiBuffer[i].Attributes = 0x00f0;
-	}
-
-	TGE::g_chiBuffer[79].Char.AsciiChar = '@';
-	TGE::g_chiBuffer[79].Attributes = 0x00f0;
-
-	TGE::g_chiBuffer[80].Char.AsciiChar = '#';
-	TGE::g_chiBuffer[80].Attributes = 0x00f0;
-	*/
-
 	//80 * 25
 	for (int i = 0; i < 2000; i++) {
 		TGE::g_chiBuffer[i].Char.AsciiChar = ' ';
@@ -36,13 +11,53 @@ void sample3()
 	}
 
 
+	TGE::g_chiBuffer[0].Char.AsciiChar = ' ';
+	TGE::g_chiBuffer[0].Attributes = 0x00a0;
 
-	/*for (int y = 0; y < 4; y++) {
+	TGE::g_chiBuffer[79].Char.AsciiChar = ' ';
+	TGE::g_chiBuffer[79].Attributes = 0x00a0;
+
+	//(x,y) => y*80 + x
+	TGE::g_chiBuffer[81].Char.AsciiChar = ' '; //80*1 + 1 => (1,1)
+	TGE::g_chiBuffer[81].Attributes = 0x00c0;
+
+	int _matObj1[4][4] = {
+		0,1,0,0,
+		1,1,1,0,
+		0,1,0,0,
+		1,1,1,1
+
+	};
+	//0,0
+	for (int y = 0; y < 4; y++) {
 		for (int x = 0; x < 4; x++) {
-			TGE::g_chiBuffer[y*80 + x].Char.UnicodeChar = _matObj1[y][x];
-			TGE::g_chiBuffer[y * 80 + x].Attributes = 0x
+			if (_matObj1[y][x] == 1) {
+				TGE::g_chiBuffer[y*80+x].Char.AsciiChar = ' ';
+				TGE::g_chiBuffer[y*80+x].Attributes = 0x00c0;
+			}
 		}
-	}*/
+	}
+	//10,0
+	for (int y = 0; y < 4; y++) {
+		for (int x = 0; x < 4; x++) {
+			if (_matObj1[y][x] == 1) {
+				int _nIndex = (y+0) * 80 + (x+10);
+				TGE::g_chiBuffer[_nIndex].Char.AsciiChar = ' ';
+				TGE::g_chiBuffer[_nIndex].Attributes = 0x00c0;
+			}
+		}
+	}
+	//5,4
+	for (int y = 0; y < 4; y++) {
+		for (int x = 0; x < 4; x++) {
+			if (_matObj1[y][x] == 1) {
+				int _nIndex = (y + 4) * 80 + (x + 5);
+				TGE::g_chiBuffer[_nIndex].Char.AsciiChar = ' ';
+				TGE::g_chiBuffer[_nIndex].Attributes = 0x00c0;
+			}
+		}
+	}
+
 
 	TGE::updateBuffer(_hStdout, TGE::g_chiBuffer);
 }
