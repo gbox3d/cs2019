3b699bfd36a88ff4c20510ef4ea43acbf52ed229
gbox3d <gbox3d@gmail.com>
2019-07-08 16:19:25 +0900

    ....

---
+++ day11/exam5/exam5.cpp
@@ -0,0 +1,24 @@
+﻿// exam5.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	char* ptr=NULL;
+	//char ptr[256];
+
+	ptr = (char *)malloc(256);
+
+	if (ptr != NULL) {
+		gets_s(ptr, 256);
+		printf_s("%s \n", ptr);
+		free(ptr);
+	}
+	else {
+		puts("메모리 확보에 실패 했습니다.");
+	}
+	
+
+	return 0;
+}
