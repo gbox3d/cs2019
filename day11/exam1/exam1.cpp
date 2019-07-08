ac2b521ad74a28dcb7c19a6c2cceb31e9c6db545
gbox3d <gbox3d@gmail.com>
2019-07-08 10:54:57 +0900

    day11

---
+++ day11/exam1/exam1.cpp
@@ -0,0 +1,28 @@
+﻿// exam1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int val = 10;
+	int* ip;
+
+	ip = &val;
+
+	printf_s("%d \n", *ip);
+
+	val = 3;
+	//*ip = 3;
+	printf_s("%d \n", *ip);
+
+	int** ipp;
+	ipp = &ip;
+	val = 2;
+
+	printf_s("%d \n", **ipp);
+	//val ,*ip, **ipp 같음 값
+
+
+	return 0;
+}
