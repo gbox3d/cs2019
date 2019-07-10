500cfacd8f782c7a3f425671e1bb748a3318d7ff
gbox3d <gbox3d@gmail.com>
2019-07-10 10:02:08 +0900

    day13

---
+++ day13/exam1/exam1.cpp
@@ -0,0 +1,25 @@
+﻿// exam1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	FILE* fp;
+	fopen_s(&fp, "test.txt", "r");
+
+	if (fp == NULL) {
+		puts("파일을 찾을수 없습니다.");
+	}
+	else {
+		puts("파일 오픈 성공");
+
+		char szBuf[256];
+		fgets(szBuf, sizeof(szBuf), fp);
+		puts(szBuf);
+
+		fclose(fp);
+	}
+
+	return 0;
+}
