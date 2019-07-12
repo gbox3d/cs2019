ce33a8384ad56831a2eb4960fda38a9166201243
seulbinim <seulbinim@gmail.com>
2019-07-12 15:08:32 +0900

    ....

--- day15/exam1/exam1.cpp
+++ day15/exam1/exam1.cpp
@@ -2,19 +2,63 @@
 //
 
 #include <iostream>
+#include "..\..\..\\cstudy\engine\irrlicht\include\irrlicht.h"
+#include "..\..\\..\\cstudy\engine\tge.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	HANDLE hStdout;
+	irr::core::vector2df test;
+
+	
+	
+
+	TGE::startTGE(&hStdout);
+	bool bLoop = true;
+
+
+	//TGE::clearScreenBuffer(TGE::g_chiBuffer, 0x0020, 0x0000);
+
+	//TGE::setCharacter(TGE::g_chiBuffer, (int)test.X, (int)test.Y, 0x0020, 0x00f0);
+
+	//test += irr::core::vector2df(2, 2);
+	//TGE::setCharacter(TGE::g_chiBuffer, (int)test.X, (int)test.Y, 0x0020, 0x00c0);
+
+	
+	test = irr::core::vector2df(40, 12);//x:40,y:12
+	irr::core::vector2df vDir(1, 0);
+	irr::f32 fAngle = 0.0;
+	while (bLoop)
+	{
+		if (TGE::input::g_KeyTable[VK_RETURN])
+		{
+			bLoop = false;
+		}
+		else if (TGE::input::g_KeyTable[VK_SPACE])
+		{
+			irr::core::vector2df _vDir = vDir;
+			_vDir.rotateBy(fAngle);
+
+			TGE::input::g_KeyTable[VK_SPACE] = false;
+			test += _vDir;
+		}
+		else if(TGE::input::g_KeyTable[VK_LEFT])
+		{
+			TGE::input::g_KeyTable[VK_LEFT] = false;
+			fAngle += 5;			
+		}
+
+		
+		TGE::clearScreenBuffer(TGE::g_chiBuffer, 0x0020, 0x0000);
+		TGE::setCharacter(TGE::g_chiBuffer, (int)test.X, (int)test.Y, 0x0020, 0x00f0);
+		TGE::updateBuffer(hStdout, TGE::g_chiBuffer);
+
+		TGE::setCursor(hStdout, 0, 25);
+		printf_s("%4.3f                       ", fAngle);
+		//puts("엔터키를 누루면 종료합니다.");
+
+	}
+
+	TGE::endTGE();
+
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
