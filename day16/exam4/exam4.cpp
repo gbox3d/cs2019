3d74349f4bba23888285b47b52fe58cbec0c3038
gbox3d <gbox3d@gmail.com>
2019-07-15 14:46:03 +0900

    ....

--- day16/exam4/exam4.cpp
+++ day16/exam4/exam4.cpp
@@ -1,10 +1,14 @@
-﻿// exam3.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+﻿// exam4.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
 #include "framework.h"
-#include "exam3.h"
+#include "exam4.h"
+
+//#define MY_DEBUG 1
 
 #define MAX_LOADSTRING 100
+
+
 
 // 전역 변수:
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
@@ -29,7 +33,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_EXAM3, szWindowClass, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_EXAM4, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
     // 애플리케이션 초기화를 수행합니다:
@@ -38,7 +42,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         return FALSE;
     }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_EXAM3));
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_EXAM4));
 
     MSG msg;
 
@@ -73,10 +77,10 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.cbClsExtra     = 0;
     wcex.cbWndExtra     = 0;
     wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_EXAM3));
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_EXAM4));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_EXAM3);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_EXAM4);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
@@ -105,19 +109,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
       return FALSE;
    }
 
-   CreateWindow(L"edit", NULL,
-	   WS_CHILD | WS_VISIBLE | WS_BORDER | CBS_AUTOHSCROLL,
-	   0, 0, 256, 32,
-	   hWnd, (HMENU)1001, hInst, NULL
-   );
-
-   CreateWindow(L"button", L"Ok", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-	   0, 64, 64, 16,
-	   hWnd, (HMENU)1002, hInst, NULL
-   );
-
-
-
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
 
@@ -144,42 +135,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             // 메뉴 선택을 구문 분석합니다:
             switch (wmId)
             {
-			case 1002:
-			{
-				HDC hDC = GetDC(hWnd);
-
-				TCHAR szBuf[256];
-
-				GetWindowText(GetDlgItem(hWnd, 1001), szBuf, sizeof(szBuf)/sizeof(TCHAR));
-				int _data = _wtoi(szBuf); //문자열데이터를 정수로 변환 
-
-				TCHAR szTemp[256];
-				swprintf_s(szTemp, L"%d", _data);
-				TextOut(hDC, 0, 200, szTemp, wcslen(szTemp));
-
-				TCHAR szTemp2[256];
-
-				for (int i = 0; i < 32; i++) {
-					int _bitShift = (_data << i);
-					if (_bitShift & 0x80000000) { // 1000 0000 ..... 32bit
-						szTemp2[i] = L'1';
-					}
-					else {
-						szTemp2[i] = L'0';
-					}
-				}
-				szTemp2[32] = L'\x00';
-
-				TextOut(hDC, 0, 232, szTemp2, wcslen(szTemp2));
-
-				//int _temp = 0;
-				//_temp = _temp | WS_POPUP
-
-
-				ReleaseDC(hWnd,hDC);
-
-			}
-				break;
             case IDM_ABOUT:
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
                 break;
@@ -196,6 +151,33 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
+			/*
+			1000 | 0100 | 0010 = 1110
+			*/
+			int flag = WS_POPUP | WS_CHILD | WS_MINIMIZE;
+			//flag = WS_CHILD;
+
+			TCHAR szTemp2[256];
+			for (int i = 0; i < 32; i++) {
+				int _bitShift = (flag << i);
+				if (_bitShift & 0x80000000) { // 1000 0000 ..... 32bit
+					szTemp2[i] = L'1';
+				}
+				else {
+					szTemp2[i] = L'0';
+				}
+			}
+			szTemp2[32] = L'\x00';
+			TextOut(hdc, 0, 0, szTemp2,wcslen(szTemp2));
+
+			swprintf_s(szTemp2, sizeof(szTemp2)/sizeof(TCHAR),L"%d", MAX_LOADSTRING);
+			TextOut(hdc, 0, 16, szTemp2, wcslen(szTemp2));
+#ifdef MY_DEBUG
+			OutputDebugString(L"debug 예제 \n");
+#endif
+
+
             EndPaint(hWnd, &ps);
         }
         break;
