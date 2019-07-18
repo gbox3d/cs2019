44f69ec8bacc855be37ca16b29a8b48ede530fe8
gbox3d <gbox3d@gmail.com>
2019-07-18 14:53:33 +0900

    ....

---
+++ day19/exam3/exam3.cpp
@@ -0,0 +1,51 @@
+﻿// exam3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+#include <stdlib.h>
+#include <WinSock2.h>
+#include  <Ws2tcpip.h>
+
+#pragma comment(lib,"ws2_32.lib")
+
+#define SERVER "192.168.0.15"
+#define BUFLEN 512
+#define PORT 3333
+
+int main()
+{
+	sockaddr_in si_other;
+	int s, slen;
+	slen = sizeof(si_other);
+
+	char buf[BUFLEN];
+	char message[BUFLEN];
+	WSADATA wsa;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		printf_s("WSAStartup() error code : %d\n", WSAGetLastError());
+		return EXIT_FAILURE;
+	}
+
+	s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (s == SOCKET_ERROR) {		
+		printf_s("socket() error code : %d\n", WSAGetLastError());
+		return EXIT_FAILURE;
+	}
+
+	memset((char*)& si_other, 0, slen);
+
+	InetPton(AF_INET, L"192.168.0.15", &si_other.sin_addr.s_addr);
+	//si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(PORT);
+
+	while (1)
+	{
+		puts("메씨지를 보내세요.");
+		gets_s(message);
+		sendto(s, message, strlen(message), 0, (sockaddr*)& si_other, slen);
+
+	}
+}
---
+++ day19/exam4/udp1.js
@@ -0,0 +1,11 @@
+//console.log('hello');
+//console.log(1+2 + "hello");
+var dgram = require("dgram");
+var udp_scoket = dgram.createSocket("udp4");
+
+console.log("wait message....");
+udp_scoket.on("message",function(msg,rinfo) {
+  console.log(msg)
+});
+
+udp_scoket.bind(3333);
