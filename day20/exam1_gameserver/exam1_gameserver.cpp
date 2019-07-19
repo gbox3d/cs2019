7d67da8c5557d76d6b07a45d69bf65c8faf7cc41
gbox3d <gbox3d@gmail.com>
2019-07-19 10:18:56 +0900

    .....

---
+++ day20/exam1_gameserver/exam1_gameserver.cpp
@@ -0,0 +1,71 @@
+﻿// exam1_gameserver.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+#include <stdlib.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib,"ws2_32.lib")
+
+int main()
+{
+	SOCKET hServerSocket;
+
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		printf("WSAStartup %d \n", WSAGetLastError());
+		return EXIT_FAILURE;
+	}
+	
+	//소켓오픈 
+	hServerSocket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (hServerSocket == INVALID_SOCKET) {
+		printf("INVALID_SOCKET %d \n", WSAGetLastError());
+		return EXIT_FAILURE;
+	}
+
+	sockaddr_in si_severAddr;
+	memset(&si_severAddr,0,sizeof(sockaddr_in));
+
+	//서버역활을 하기위한 주소 정보 셋업
+	si_severAddr.sin_family = AF_INET;
+	si_severAddr.sin_port = htons(3333); //htons,리틀엔디언 => 빅엔디언
+	si_severAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	if (
+		bind(hServerSocket, (sockaddr*)& si_severAddr, sizeof(sockaddr_in)) == SOCKET_ERROR
+		)
+	{
+		printf("bind error %d \n", WSAGetLastError());
+		return EXIT_FAILURE;
+	}
+
+	while (1)
+	{
+		sockaddr_in si_other;
+
+		memset(&si_other, 0, sizeof(sockaddr_in));
+		int slen = sizeof(sockaddr_in);
+
+		puts("wait request...");
+		fflush(stdout);
+		char szBuf[1024];
+		memset(szBuf, 0, sizeof(szBuf));
+
+		int recv_len = recvfrom(hServerSocket, szBuf, sizeof(szBuf), 0,
+			(sockaddr*)& si_other, &slen);
+
+		char _szIp[256];
+		inet_ntop(AF_INET, &si_severAddr.sin_addr, _szIp, sizeof(_szIp));
+		printf_s("from : %s : %d", _szIp, 
+			ntohs(si_other.sin_port) //ntohs,빅엔디언 => 리틀엔디언
+		);
+
+	}
+
+	closesocket(hServerSocket);
+	WSACleanup();
+
+}
---
+++ day20/readme.txt
@@ -0,0 +1,10 @@
+exam1 udp 게임서버
+
+p2p => udp 만으로 하지않는다. tcp로도 가능하나, 불편하기 때문에
+udp를 선호한다.
+
+cs(client , server) => 
+client 요청(master,guest), server 응답(slave,host)
+
+sockaddr_in => 주소정보구조체
+
